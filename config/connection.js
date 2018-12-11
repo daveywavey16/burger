ee2883d0f87508f561388aaa217a772a13d540dc
daveywavey16 <daveywavey16@msn.com>
2018-12-11 11:20:15 -0700

    updated database connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "damnburger.herokuapp.com",
-  //port: 3306,
-  user: "ba11fff6150568",
-  password: "dadd457b",
-  database: "heroku_ae28d2767e94a53"
+  host: "xefi550t7t6tjn36.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "m5owu0sjmn9nlme5",
+  password: "ydyrvzn88eb61gzf",
+  database: "uwef5fjv3hwjbte6"
 });
 
 connection.connect(function(err) {
