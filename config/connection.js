3f451e359bb645b657365e30e1741bee19ca2159
daveywavey16 <daveywavey16@msn.com>
2018-12-11 11:24:16 -0700

    updated database connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,13 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JWS_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	
+}
+connection = mysql.createConnection({
   host: "xefi550t7t6tjn36.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   user: "m5owu0sjmn9nlme5",
